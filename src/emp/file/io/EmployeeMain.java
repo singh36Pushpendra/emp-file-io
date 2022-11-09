b035869561bfa1d5bf4c32faf40d8851d7efe69b
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-09 17:20:06 +0530

    Added: UC1 Write employee data to file.

--- src/emp/file/io/EmployeeMain.java
+++ src/emp/file/io/EmployeeMain.java
@@ -1,7 +1,34 @@
 package emp.file.io;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
 public class EmployeeMain {
     public static void main(String[] args) {
         System.out.println("Welcome to File IO!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Employee Id: ");
+        int empId = scanner.nextInt();
+        System.out.print("Enter Employee Name: ");
+        String empName = scanner.next();
+        System.out.print("Enter Employee Salary: ");
+        double empSalary = scanner.nextDouble();
+
+        Employee emp = new Employee(empId, empName, empSalary);
+
+        Path path = Paths.get("C:/Users/micro/Downloads/emp-info.txt");
+
+        try {
+            Files.writeString(path, emp.toString(), StandardOpenOption.CREATE);
+            System.out.println("Employee information saved successfully to file!\n" + path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
