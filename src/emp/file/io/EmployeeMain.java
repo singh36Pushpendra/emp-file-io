71eb215feb5425dc734468f7eb2bb20abd480109
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-10 22:31:12 +0530

    Added: UC4 Stored employees to file

--- src/emp/file/io/EmployeeMain.java
+++ src/emp/file/io/EmployeeMain.java
@@ -5,28 +5,61 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class EmployeeMain {
+
+    private static int empId;
+    private static String empName;
+    private static double empSalary;
+    private static Scanner scanner;
+
+    private static void inputEmpPayroll() {
+
+        System.out.print("Enter Employee Id: ");
+        empId = scanner.nextInt();
+        System.out.print("Enter Employee Name: ");
+        empName = scanner.next();
+        System.out.print("Enter Employee Salary: ");
+        empSalary = scanner.nextDouble();
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to File IO!");
 
-        Scanner scanner = new Scanner(System.in);
+        String option;
+        scanner = new Scanner(System.in);
 
-        System.out.print("Enter Employee Id: ");
-        int empId = scanner.nextInt();
-        System.out.print("Enter Employee Name: ");
-        String empName = scanner.next();
-        System.out.print("Enter Employee Salary: ");
-        double empSalary = scanner.nextDouble();
+        List<Employee> employees = new ArrayList<>();
 
-        Employee emp = new Employee(empId, empName, empSalary);
+        while (true) {
+            inputEmpPayroll();
+            employees.add(new Employee(empId, empName, empSalary));
+
+            System.out.print("Do you wan't to add one more Employee entry[Yes/No]: ");
+            option = scanner.next();
+
+            if (option.equalsIgnoreCase("nO"))
+                break;
+        }
 
         Path path = Paths.get("C:/Users/micro/Downloads/emp-info.txt");
 
+        List<String> empsList = employees
+                .stream().map(emp -> emp.toString())
+                .collect(Collectors.toList());
+
         try {
-            Files.writeString(path, emp.toString(), StandardOpenOption.CREATE);
-            System.out.println("Employee information saved successfully to file!\n" + path);
+            if (! Files.exists(path))
+                Files.createFile(path);
+
+            Files.write(path, empsList, StandardOpenOption.APPEND);
+            System.out.println(employees.size() + " Employee information saved successfully to file!\n" + path);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
